a82e254ce3f55f0d8443584ebe3f2bfc9ca4718a
jh <jhmailnet@gmail.com>
2017-04-11 08:58:34 +0800

    myweb

---
+++ manage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding:utf-8
+import os
+from app import create_app, db
+from app.models import User, Role, Permission, Post
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    '''
+    功能：为shell命令添加一个上下文
+    '''
+
+    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission,
+                Post=Post)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """运行测试"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
